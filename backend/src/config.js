5d0e9ae98b993e0aa1488426308b3b26c7c53730
Jose luis Paredes muñoz <joseluisparedesmunoz@MacBook-Pro-de-Jose.local>
2022-02-12 22:58:05 +0100

    created .env

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,6 +4,6 @@ dotenv();
 export const config = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "fide0000",
+  password: process.env.DB_PASSWORD || "password",
   database: process.env.DB_DATABASE || "test",
 };
