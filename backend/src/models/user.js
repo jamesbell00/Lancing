4a46b9725329eb3773289139ef21c3af8ac08e2a
Jose luis Paredes muñoz <joseluisparedesmunoz@MacBook-Pro-de-Jose.local>
2022-02-25 20:46:09 +0100

    made changes in user

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,13 +1,13 @@
-//import {connect} from '../database.js'
+import {connect} from '../database.js'
 
-// const User = define('users', {
-// email: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//  },
-// password: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//  },
-// });
-// export default User;
+const User = define('user', {   
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+});
+export default User;
